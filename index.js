f60cd9cf011d8138c0287f23f91f04aa7e88ecda
+ <+>
2022-10-30 04:24:44 -0400

    commit tree b932ab2c42608cb6aaef8972be6703a3e6ac21ce

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+
+function submitData(name, email) {
+    const userObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`,
+        }),
+    };
+    return fetch("http://localhost:3000/users", userObj)
+    .then(resp => resp.json())
+    .then(body => newUser(body.id))
+    .catch(error => document.body.innerHTML = error.message)
+}
+
+function newUser(newID){
+    document.getElementsByTagName("body")[0].innerHTML = newID;
+    document.body.append(newID)
+}
